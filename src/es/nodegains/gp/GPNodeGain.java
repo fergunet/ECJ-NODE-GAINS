8a1a1d35146dbac70bfb74ebd2cbfe26666c2eb2
Pablo García Sánchez <fergunet@gmail.com>
2018-11-06 13:29:11 +0100

    Added functions adapted to use the gain

---
+++ src/es/nodegains/gp/GPNodeGain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.nodegains.gp;
+
+import ec.gp.GPNode;
+
+/**
+ *
+ * @author pgarcia
+ */
+public abstract class GPNodeGain extends GPNode{
+    private double gain;
+
+    public double getGain() {
+        return gain;
+    }
+
+    public void setGain(double gain) {
+        this.gain = gain;
+    }
+    
+    
+}
