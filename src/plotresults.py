f1c0854a882349b095f17476183efe79a0701db2
Pablo García Sánchez <fergunet@gmail.com>
2018-12-17 19:40:36 +0100

    Added problem list in plot-results

--- src/plotresults.py
+++ src/plotresults.py
@@ -5,22 +5,19 @@ import os
 
 
 
-base_directory = "/home/pgarcia/NetBeansProjects/ECJ-NODE-GAINS/results"
+base_directory = "/home/pgarcia/code/NODEGAINSCLUSTER/"
 files = []
-problems = ["koza-2", "koza-3"]
-
-"""
-"koza-1",
-"koza-2",
-"koza-3",
-"nguyen-1",
-"nguyen-2",
-"nguyen-3",
-"nguyen-4",
+problems = [
+#"koza-1",
+#"koza-2",
+#"koza-3",
+#"nguyen-1",
+#"nguyen-2",
+#"nguyen-3",
+#"nguyen-4",
 "nguyen-5",
 "nguyen-6",
-"nguyen-7",
-"nguyan-8",
+#"nguyen-7", #nguyen8 do not exist
 "nguyen-9",
 "nguyen-10",
 "pagie-1",
@@ -41,29 +38,29 @@ problems = ["koza-2", "koza-3"]
 "korns-14",
 "korns-15",
 "keijzer-1",
-"keijzer-2",
+#"keijzer-2",
 "keijzer-3",
 "keijzer-4",
 "keijzer-5",
-"keijzer-6",
+#"keijzer-6",
 "keijzer-7",
 "keijzer-8",
 "keijzer-9",
 "keijzer-10",
-"keijzer-11",
-"keijzer-12",
+#"keijzer-11",
+#"keijzer-12",
 "keijzer-13",
-"keijzer-14",
-"keijzer-15",
+#"keijzer-14",
+#"keijzer-15",
 "vladislavleva-1",
 "vladislavleva-2",
 "vladislavleva-3",
 "vladislavleva-4",
 "vladislavleva-5",
-"vladislavleva-6",
+#"vladislavleva-6",
 "vladislavleva-7",
-"vladislavleva-8",
-"""
+#"vladislavleva-8",
+]
 
 
 
@@ -71,7 +68,7 @@ def obtain_df_from_dir(directory):
 	df = pd.DataFrame(columns=('JOB','PROBLEM','GENS','TOTAL_TIME', 'EVAL_TIME','BEST','AVERAGE_END','NODES_BEST'))
 	for p in problems:
 		#print p
-		for i in range(0,28):
+		for i in range(0,20):
 			filename = directory+"/job."+str(i)+"."+p+".gens"
 			#print("Opening file "+filename)
 			with open(filename, 'rb') as f:
@@ -84,7 +81,7 @@ def obtain_df_from_dir(directory):
 				gens = 0.0
 				size_best = 0
 				for row in reader:
-					time = float(row[0]) + time
+					time = float(row[0])
 					eval_time = float(row[1]) + eval_time
 					size_best = float(row[4])
 					average_gen = float(row[5])
@@ -93,15 +90,14 @@ def obtain_df_from_dir(directory):
 				df.loc[len(df)] = [i, p, gens, time, eval_time, best, average_gen, size_best]
 	return df
 
+
 def print_results(dataframe):
-	print(dataframe)
+	pd.options.display.float_format = '{:20,.10f}'.format
+	#print(dataframe)
 	print(dataframe.groupby('PROBLEM').mean())
 
-df_sa = obtain_df_from_dir(base_directory+"/sa")
-df_nosa = obtain_df_from_dir(base_directory+"/without_sa")
+df_sa = obtain_df_from_dir(base_directory+"/NONE/classes")
+df_nosa = obtain_df_from_dir(base_directory+"/SA/classes")
 
 print_results(df_sa)
 print_results(df_nosa)
-
-
-			
