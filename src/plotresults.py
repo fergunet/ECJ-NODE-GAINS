bec7a037c7528e8085152b9983497585d8438d25
Pablo García Sánchez <fergunet@gmail.com>
2019-01-28 15:27:24 +0100

    Final version of plot results to get the results

--- src/plotresults.py
+++ src/plotresults.py
@@ -6,20 +6,21 @@ import re
 
 
 
-base_directory = "/home/pgarcia/code/NODEGAINSINCLUSTER"
+base_directory = "/home/pgarcia/code/NODEGAINSCLUSTER"
 
 files = []
 problems = [
-#"koza-1",
-#"koza-2",
-#"koza-3",
-#"nguyen-1",
-#"nguyen-2",
-#"nguyen-3",
-#"nguyen-4",
+"koza-1",
+"koza-2",
+"koza-3",
+"nguyen-1",
+"nguyen-2",
+"nguyen-3",
+"nguyen-4",
 "nguyen-5",
 "nguyen-6",
-#"nguyen-7", #nguyen8 do not exist
+"nguyen-7",
+"nguyen-8",
 "nguyen-9",
 "nguyen-10",
 "pagie-1",
@@ -40,28 +41,28 @@ problems = [
 "korns-14",
 "korns-15",
 "keijzer-1",
-#"keijzer-2",
+"keijzer-2",
 "keijzer-3",
 "keijzer-4",
 "keijzer-5",
-#"keijzer-6",
+"keijzer-6",
 "keijzer-7",
 "keijzer-8",
 "keijzer-9",
 "keijzer-10",
-#"keijzer-11",
-#"keijzer-12",
+"keijzer-11",
+"keijzer-12",
 "keijzer-13",
-#"keijzer-14",
-#"keijzer-15",
+"keijzer-14",
+"keijzer-15",
 "vladislavleva-1",
 "vladislavleva-2",
 "vladislavleva-3",
 "vladislavleva-4",
 "vladislavleva-5",
-#"vladislavleva-6",
+"vladislavleva-6",
 "vladislavleva-7",
-#"vladislavleva-8",
+"vladislavleva-8",
 ]
 
 
@@ -101,6 +102,7 @@ def obtain_df_from_dir(directory):
 					f.seek(-2, os.SEEK_CUR)  # ...jump back the read byte plus one more.
 				last = f.readline()
 				last_info = re.split(" |=|\n", last)
+				#print(filenameStats)
 				validation_standarized = float(last_info[2])
 				validation_adjusted = float(last_info[4])
 				validation_hits= float(last_info[6])
@@ -113,13 +115,13 @@ def obtain_df_from_dir(directory):
 def print_results(dataframe):
 	pd.options.display.float_format = '{:20.10f}'.format
 	#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-	print(dataframe.to_string())
-	#print(dataframe.groupby('PROBLEM').mean().to_string())
+	#print(dataframe.to_string())
+	print(dataframe.groupby('PROBLEM').std().to_string())
 
 df_nosa = obtain_df_from_dir(base_directory+"/NONE/classes")
 df_sa = obtain_df_from_dir(base_directory+"/SA/classes")
 
 print("NONE")
 print_results(df_nosa)
-#print("SA")
-#print_results(df_sa)
+print("SA")
+print_results(df_sa)
